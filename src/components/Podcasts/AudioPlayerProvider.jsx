4006c00ea59f93b875c94b3f9e327ca5218924f9
Brooklyn <markmclean2005@gmail.com>
2026-04-12 11:19:38 +0200

    added confirmation to reload while audio open

--- src/components/Podcasts/AudioPlayerProvider.jsx
+++ src/components/Podcasts/AudioPlayerProvider.jsx
@@ -1,6 +1,28 @@
+import { useEffect } from "react";
 import AudioPlayer from "./AudioPlayer.jsx";
+import { useAudioPlayerStore } from "../../store/audioPlayerStore.js";
 
 export default function AudioPlayerProvider({ children }) {
+  const currentTrack = useAudioPlayerStore((state) => state.currentTrack);
+
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      if (!currentTrack?.src) {
+        return;
+      }
+
+      const message = "Your audio playback is active. Are you sure you want to leave?";
+      event.preventDefault();
+      event.returnValue = message;
+      return message;
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [currentTrack]);
+
   return (
     <>
       <div className="audio-player-provider">{children}</div>
